5eab38319af72467b24b7276eaf4debd872e6af5
Nihal Patidar <nihalpatidar14@gmail.com>
2026-06-17 17:25:44 +0530

    feat : establish Mongodb connection with mongoose

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express' ;
 import dotenv from 'dotenv' ;
+import mongoose from 'mongoose';
 
 
 dotenv.config(); // intializing dotenv
@@ -7,6 +8,10 @@ dotenv.config(); // intializing dotenv
 const app = express(); // initializing a instance of a server
 
 const PORT = process.env.PORT || 5000 ; // getting port number from .env file
+
+mongoose.connect(process.env.DB_URL).then(()=>{
+    console.log("database connected successfully");
+})
 
 app.listen(PORT,()=>{
     console.log("Server is listening on ", PORT);
