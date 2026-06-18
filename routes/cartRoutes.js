736caedbf5f63a3e94a5723eede60d3bbcff3938
Nihal Patidar <nihalpatidar14@gmail.com>
2026-06-18 17:49:59 +0530

    security : add authentication to cart routes

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -4,11 +4,11 @@ import auth from '../middlewares/authentication.js';
 
 const cartRoutes = express.Router();
 
+// add authentication to all cart routes
 cartRoutes.post('/cart',auth,addToCart);
 
 cartRoutes.put('/cart',auth,updateToCart);
 
 cartRoutes.delete('/cart',auth,deleteFromCart);
-
 
 export default cartRoutes ;
